be80426811e0522f11392c947d3b4ebb9465c5e5
Umhar Hussain <130864554+Umhar97@users.noreply.github.com>
2023-09-03 16:24:23 +0100

    added html elements to javascript logic page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,11 @@
 let currentQuestionIndex = 0;
 let time = questions.length * 15;
 let timerID;
+
+let timerElement = document.getElementById("time");
+let questionsElement = document.getElementById("questions");
+let choicesElement = document.getElemtnById("choices");
+let submitButton = document.getElementById("submit");
+let startButton = document.getElementById("start");
+let initialsElement = document.getElementById("initials");
+let feedbackElement = document.getElementById("feedback");
