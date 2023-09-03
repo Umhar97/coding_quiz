6544ed2987c5e3f1714b798710f63a0b6a7e0f25
Umhar Hussain <130864554+Umhar97@users.noreply.github.com>
2023-09-03 17:01:25 +0100

    corrected spelling error in logic.js line 7

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,10 +4,11 @@ let timerID;
 
 let timerElement = document.getElementById("time");
 let questionsElement = document.getElementById("questions");
-let choicesElement = document.getElemtnById("choices");
+let choicesElement = document.getElementById("choices");
 let submitButton = document.getElementById("submit");
 let startButton = document.getElementById("start");
 let initialsElement = document.getElementById("initials");
 let feedbackElement = document.getElementById("feedback");
 
 let sxfCorrect = new Audio("assets/sfx/correct.wav");
+
