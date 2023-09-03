e015a1676398834bf620a5bd623984eea99d257d
Umhar Hussain <130864554+Umhar97@users.noreply.github.com>
2023-09-03 16:12:56 +0100

    added second question to questions.js

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,4 +4,9 @@ let questions = [
         "choices": ["string", "booleans", "alerts", "numbers"],
         "answer": "alerts"
     }, 
+    {
+        "title": "Conditionals keywords are:",
+        "choices": ["while", "for", "if", "for each"],
+        "answer":"if"
+    },
 ]
